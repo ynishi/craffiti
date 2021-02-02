bb31b572bd6c5ac3e5ea2041e10f9dd65cc61c4b
Yutaka Nishimura <ytk.nishimura@gmail.com>
2021-02-03 02:10:52 +0900

    add project name

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,12 +12,16 @@ import qualified Turtle as T
 
 import Data.ByteString.UTF8 as BSU
 
+import Prep
+
 data Opt = Opt
   { optCommand :: Command
   }
 
-data Command =
-  New String
+data Command = New
+  { projectName :: String,
+    init :: Bool}
+
 
 optParser :: Parser Opt
 optParser =
@@ -27,6 +31,7 @@ optParser =
 createOptions :: Parser Command
 createOptions =
   New <$> strArgument (metavar "projectName" <> help "project name for create")
+      <*> switch (long "init" <> short 'i' <> help "whether to only init")
 
 parse :: IO Opt
 parse = execParser opts
@@ -43,12 +48,18 @@ projectDirs = ["", ".craffiti", "front", "server", "ai", "batch"]
 run :: Opt -> IO ()
 run opt =
   case optCommand opt of
-    New projectName ->
+    New projectName init ->
       runSimpleApp $ do
         logInfo $
           displayBytesUtf8 $
           BSU.fromString $ "Create new project: " ++ projectName
         let projectPath = T.decodeString projectName
         mapM_ (\d -> T.mkdir $ projectPath T.</> d) projectDirs
+        T.cd projectPath
+        T.cd "front"
+        liftIO $ runPrep init (reactProgram projectName)
+        T.cd ".." >> T.cd "server"
+        liftIO $ runPrep init (stackProgram projectName)
+        T.cd ".."
         logInfo $
           displayBytesUtf8 $ BSU.fromString "success, Have a fan to craft!"
--- src/Prep.hs
+++ src/Prep.hs
@@ -11,6 +11,7 @@ import qualified Turtle as T
 import qualified Control.Foldl as Fold
 
 import Control.Monad.Operational as O
+import Data.List (intercalate)
 
 -- GADTs style Prep include main tasks
 -- when integrate with Applicative, make instance of it, but GADTs is not supported default Functor, use Coyoneda.Functor
@@ -19,11 +20,13 @@ import Control.Monad.Operational as O
 -- https://jakewheat.github.io/intro_to_parsing/#getting-started parsec support to Applicative, Parser is instance of Applicative.
 
 data Prep a where
+  Require :: String -> Prep(String, Int)
   Init :: String -> Prep (String, Int)
   Install :: String -> FilePath -> Prep (String, Int)
   Check :: [Int] -> Prep (String, Int)
 
 instance Show (Prep a) where
+  show (Require x) = x
   show (Init x) = x
   show (Install x f) = x ++ show f
   show (Check x) = show x
@@ -37,20 +40,30 @@ reactProgram projectName = do
   (_, i) <- singleton $ Check [xi, yi]
   return (x++y, i)
 
-runPrep :: Program Prep a -> IO a
-runPrep = interpretWithMonad eval where
+stackProgram :: ProjectName -> Program Prep (String,Int)
+stackProgram projectName = do
+  (x, xi) <- singleton . Init . unwords $ ["stack", "new", projectName]
+  (y, yi) <- singleton $ Install (unwords ["stack", "build"]) projectName
+  (_, i) <- singleton $ Check [xi, yi]
+  return (x++y, i)
+
+-- TODO: integrate with RIO with Operational
+runPrep :: Bool -> Program Prep a -> IO a
+runPrep initOpt = interpretWithMonad eval where
   eval :: Prep a -> IO a
   eval (Init x) = do
-    T.cd "front"
     runSimpleApp . logInfo . displayBytesUtf8 . BSU.fromString $ "start init:" ++ x
     res <- Prep.get (T.fromString x)
     runSimpleApp . logInfo . displayBytesUtf8 . BSU.fromString $ "end init:" ++ x ++ show res
     return  (show res, 0)
   eval (Install x _) = do
-    runSimpleApp . logInfo . displayBytesUtf8 . BSU.fromString $ "start install:" ++ x
-    res <- Prep.get (T.fromString x)
-    runSimpleApp . logInfo . displayBytesUtf8 . BSU.fromString $ "end install:" ++ x ++ show res
-    return  (show res, 0)
+      res <- if initOpt then
+         runSimpleApp . logInfo . displayBytesUtf8 . BSU.fromString $ "option init set, don't install:" ++ x
+      else do
+         runSimpleApp . logInfo . displayBytesUtf8 . BSU.fromString $ "start install:" ++ x
+         res <- Prep.get (T.fromString x)
+         runSimpleApp . logInfo . displayBytesUtf8 . BSU.fromString $ "end install:" ++ x ++ show res
+      return  (show res, 0)
   eval (Check x) = do
     runSimpleApp . logInfo . displayBytesUtf8 . BSU.fromString $ "check:" ++ show x
     return (show x, sum x)
